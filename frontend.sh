d8b994333a8ede659546b00f80d13ab669a637a4
“Cole-Cyber” <coleakomolafe@gmail.com>
2023-11-05 16:59:17 -0600

    frontend.sh script-updating the dns- added color comment

--- frontend.sh
+++ frontend.sh
@@ -7,14 +7,14 @@ cp expense.conf /etc/nginx/default.d/expense.conf &>>/tmp/expense.log
 echo -e "\e[35m removing the content of the installed nginx \e[0m"
 rm -rf /usr/share/nginx/html/* &>>/tmp/expense.log
 
-echo -e "\e[32m downloading the front end of app code from developer \e[0m"
+echo -e "\e[31m downloading the front end of app code from developer \e[0m"
 curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip &>>/tmp/expense.log
 
 echo -e "\e[33m Extract downloaded content \e[0m"
 cd /usr/share/nginx/html &>>/tmp/expense.log
 unzip /tmp/frontend.zip &>>/tmp/expense.log
 
-echo -e "\e[32m enable and start the server/nginx \e[0m"
+echo -e "\e[34m enable and start the server/nginx \e[0m"
 systemctl enable nginx &>>/tmp/expense.log
 systemctl restart nginx &>>/tmp/expense.log
 
