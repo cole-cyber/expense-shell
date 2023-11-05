da94595e69a4f1606c68322a89fe84cbaa916faa
“Cole-Cyber” <coleakomolafe@gmail.com>
2023-11-05 16:50:03 -0600

    frontend.sh script-updating the dns- added color comment

--- frontend.sh
+++ frontend.sh
@@ -1,20 +1,20 @@
 echo -e "\e[36m This command installed the webserver (nginx) \e[0m"
-dnf install nginx -y
+dnf install nginx -y &>>/tmp/expense.log
 
 echo -e "e/[32m copying expense.conf file into the service file \e[0m"
-cp expense.conf /etc/nginx/default.d/expense.conf
+cp expense.conf /etc/nginx/default.d/expense.conf &>>/tmp/expense.log
 
 echo -e "e\ [35m removing the content of the installed nginx \e[0m"
-rm -rf /usr/share/nginx/html/*
+rm -rf /usr/share/nginx/html/* &>>/tmp/expense.log
 
 echo -e "\e[32m downloading the front end of app code from developer \e[0m"
-curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip
+curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip &>>/tmp/expense.log
 
 echo -e "\e[33m Extract downloaded content \e[0m"
-cd /usr/share/nginx/html
-unzip /tmp/frontend.zip
+cd /usr/share/nginx/html &>>/tmp/expense.log
+unzip /tmp/frontend.zip &>>/tmp/expense.log
 
 echo -e "\e[32m enable and start the server/nginx \e[0m"
-systemctl enable nginx
-systemctl restart nginx
+systemctl enable nginx &>>/tmp/expense.log
+systemctl restart nginx &>>/tmp/expense.log
 
