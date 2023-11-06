879d39ef00690004e96681bb304d80455da8decb
“Cole-Cyber” <coleakomolafe@gmail.com>
2023-11-06 01:15:32 -0600

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	frontend.sh

--- frontend.sh
+++ frontend.sh
@@ -1,20 +1,22 @@
+log.file=expense.log
+
 echo -e "\e[36m This command installed the webserver (nginx) \e[0m"
-dnf install nginx -y &>>/tmp/expense.log
+dnf install nginx -y &>>log.file
 
 echo -e "\e[32m copying expense.conf file into the service file \e[0m"
-cp expense.conf /etc/nginx/default.d/expense.conf &>>/tmp/expense.log
+cp expense.conf /etc/nginx/default.d/expense.conf &>>log.file
 
 echo -e "\e[35m removing the content of the installed nginx \e[0m"
-rm -rf /usr/share/nginx/html/* &>>/tmp/expense.log
+rm -rf /usr/share/nginx/html/* &>>log.file
 
 echo -e "\e[32m downloading the front end of app code from developer \e[0m"
-curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip &>>/tmp/expense.log
+curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip &>>log.file
 
 echo -e "\e[33m Extract downloaded content \e[0m"
-cd /usr/share/nginx/html &>>/tmp/expense.log
-unzip /tmp/frontend.zip &>>/tmp/expense.log
+cd /usr/share/nginx/html &>>log.file
+unzip /tmp/frontend.zip &>>log.file
 
 echo -e "\e[32m enable and start the server/nginx \e[0m"
-systemctl enable nginx &>>/tmp/expense.log
-systemctl restart nginx &>>/tmp/expense.log
+systemctl enable nginx &>>log.file
+systemctl restart nginx &>>log.file
 
